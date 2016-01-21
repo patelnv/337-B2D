fbf92bc3c407fcdf106d28bfef618a640223c570
Nilesh Kevadiya <npatel@oakland.edu>
2016-01-21 16:12:19 -0500

    Added logic to read binary number from console input

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to Binary to decimal converter!\n");
+        System.out.println("Enter Your Binary Number:");
+        Scanner scanner = new Scanner(System.in);
+        String buffer = scanner.nextLine();
+        System.out.println("You have entered: " + buffer + " as your input.");
     }
 }
